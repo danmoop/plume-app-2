943cfee02983ea30bc72e692bcc9532771888b9c
dan322 <dandurnev0@gmail.com>
2018-12-16 13:55:19 +0300

    adding, removing, renaming lists

--- src/renderer/components/EditorPage.vue
+++ src/renderer/components/EditorPage.vue
@@ -8,6 +8,9 @@
                     </button>
                     <ul class="list-reset">
                         <a class="q list-title"><i class="fas fa-file-invoice"></i> Lists</a>
+                        <button @click="addList" class="navBtn bg-white text-grey-darkest font-semibold py-2 px-4 border border-grey-light rounded shadow">
+                            <i class="fas fa-plus"></i>
+                        </button>
                         <li v-for="list in project.lists" class="q list-item">
                             <a href="#" @click="openList(list)" class="item"><i class="far fa-file"></i>
                                 {{list.title}}</a>
@@ -15,10 +18,11 @@
                     </ul>
                     <hr>
                     <ul class="list-reset under-item">
-                        <a class="q list-title"><i class="fas fa-trash"></i> Trash</a>
+                        <a class="q list-title"><i class="fas fa-trash-alt"></i> Trash</a>
                         <li v-for="list in project.trash" class="q list-item">
-                            <a href="#" class="trash-item"><i class="far fa-file"></i>
-                                {{list.name}}</a>
+                            <a @click="previewTrash(list)" href="#" class="trash-item"><i class="far fa-file"></i>
+                                {{list.title}}</a>
+                                <a href="#" @click="deleteFromTrash(list)"><i class="far fa-times-circle"></i></a>
                         </li>
                     </ul>
 
@@ -29,16 +33,32 @@
                     <br>
                 </div>
             </div>
-            <div class="w-5/6 h-12 vh-100">
-                <quill-editor v-if="editorShown" style="height: 100vh;" @ready="onEditorReady" :content="content" :options="editorOption"
-                    @change="onEditorChange($event)">
-                </quill-editor>
+            <div class="w-5/6 h-12 h-screen" style="overflow-y: scroll;">
+                <ul v-if="editorShown && !trashShown" class="list-reset flex justify-between" style="border: 2px solid #ecf0f1;">
+                    <li class="mr-3">
+                        <input @change="onNameChange($event)" class="border border-grey-light q ml-5" placeholder="Username"
+                            :value="this.currentList.title">
+                        <button @click="deleteList" class="navBtn bg-grey-light text-grey-darkest font-bold py-2 px-4 rounded inline-flex items-center">
+                            <i class="fas fa-trash-alt"></i>
+                        </button>
+                    </li>
+                    <li class="mr-3">
+                    </li>
+                    <li class="mr-3">
+                    </li>
+                </ul>
+
                 <h2 class="text-center" style="width: 90%;margin: 0px auto;margin-top: 25%;" v-if="!editorShown">Looks
                     like there is nothing here yet. Get started by creating / opening blank file on the left sidebar!</h2>
+
+                <quill-editor :disabled="disabled" v-if="editorShown" @ready="onEditorReady" :content="content"
+                    :options="editorOption" @change="onEditorChange($event)">
+                </quill-editor>
+
+                <br><br>
             </div>
         </div>
     </div>
-
 </template>
 
 <script>
@@ -70,7 +90,9 @@
                 editorShown: false,
                 wordAmount: null,
                 currentList: null,
-                fs: null
+                fs: null,
+                trashShown: false,
+                disabled: false
             }
         },
         mounted() {
@@ -90,22 +112,70 @@
                 this.editorShown = true;
                 this.content = list.content;
                 this.currentList = list;
+                this.trashShown = false;
+                this.disabled = false;
             },
-            onEditorReady(quill){
+            onEditorReady(quill) {
                 var symNum = quill.container.innerText.length;
-                var wordNum = quill.container.innerText.split(" ").length;
+                var wordNum = quill.container.innerText.trim().split(/\s+/).length;
                 this.$root.$emit('setNums', symNum, wordNum);
             },
             onEditorChange(event) {
                 this.content = event.html;
                 this.currentList.content = this.content;
 
-                this.fs.writeFile(this.project.pathFile, JSON.stringify(this.project, null, "\t"), function (err) {
-                });
+                this.fs.writeFile(this.project.pathFile, JSON.stringify(this.project, null, "\t"), function (err) {});
 
                 var symNum = event.text.length;
                 var wordNum = event.text.split(" ").length;
                 this.$root.$emit('setNums', symNum, wordNum);
+            },
+            addList() {
+                var list = {
+                    title: "Untitled",
+                    content: '',
+                    id: "list_" + this.makeid()
+                }
+
+                this.project.lists.push(list);
+
+                this.fs.writeFile(this.project.pathFile, JSON.stringify(this.project, null, "\t"), function (err) {});
+            },
+            makeid() {
+                var text = '';
+                var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+                for (var i = 0; i < 20; i++)
+                    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+                return text;
+            },
+            deleteList() {
+                this.project.trash.push(this.currentList);
+                var index = this.project.lists.indexOf(this.currentList);
+                this.project.lists.splice(index, 1);
+
+                this.currentList = null;
+                this.editorShown = false;
+
+                this.fs.writeFile(this.project.pathFile, JSON.stringify(this.project, null, "\t"), function (err) {});
+            },
+            onNameChange(event) {
+                var index = this.project.lists.indexOf(this.currentList);
+                this.project.lists[index].title = event.target.value;
+                this.fs.writeFile(this.project.pathFile, JSON.stringify(this.project, null, "\t"), function (err) {});
+            },
+            deleteFromTrash(list) { 
+                var index = this.project.trash.indexOf(list);
+                this.project.trash.splice(index, 1);
+                this.fs.writeFile(this.project.pathFile, JSON.stringify(this.project, null, "\t"), function (err) {});
+            },
+            previewTrash(list) { 
+                this.editorShown = true;
+                this.content = list.content;
+                this.currentList = list;
+                this.trashShown = true;
+                this.disabled = true;
             }
         }
     }
@@ -138,18 +208,18 @@
         color: #333;
     }
 
-    .trash-item{
+    .trash-item {
         transition: 0.25s;
         text-decoration: none;
         color: #333;
     }
 
-    .trash-item:hover{
+    .trash-item:hover {
         transition: 0.25s;
         color: #e74c3c;
     }
 
-    .under-item{
+    .under-item {
         margin-top: 50px;
     }
 
